3d6c0dc2fb02798497daa1706486c2ec5232b0a1
Kamlesh Kumar <kushwahakamleshraj434@gmail.com>
2024-09-24 00:15:29 +0530

    Every thing is okkk

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
         <img src={facebookImg} alt="tiktok" width={24} height={24} />
         <img src={youtubeImg} alt="youtube" width={24} height={24} />
       </div>
-      <div id="header-text" className={styles.logo}>MyNews</div>
+      <div id="header-text" className={styles.logo}>XENDEK WEB</div>
       <div id="header-link" className={styles.links}>
         <ThemeToggle />
         <Link  id="link"  href="/" className={styles.link}>Homepage</Link>
